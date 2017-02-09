5d4460e5b4485351f08f4943a214ea49a26ed6c5
Gabriel Matsuoka <gabriel.matsuoka@gmail.com>
2017-02-09 19:01:42 -0200

    change version 3.3.9 to 3.4.0

--- v1.6.x/mercadopago/mercadopago.php
+++ v1.6.x/mercadopago/mercadopago.php
@@ -88,7 +88,7 @@ class MercadoPago extends PaymentModule
     {
         $this->name = 'mercadopago';
         $this->tab = 'payments_gateways';
-        $this->version = '3.3.9';
+        $this->version = '3.4.0';
         $this->currencies = true;
         //$this->currencies_mode = 'radio';
         $this->need_instance = 0;
